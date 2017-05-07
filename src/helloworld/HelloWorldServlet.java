0c288a57774bf26085e350b9dc7d7805d3b57055
jerry <cle.ee@163.com>
2017-05-07 15:06:31 +0200

    java part done

--- src/helloworld/HelloWorldServlet.java
+++ src/helloworld/HelloWorldServlet.java
@@ -16,26 +16,47 @@ public class HelloWorldServlet extends HttpServlet {
         super();
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	
-    	List<String> allowedUserId = Arrays.asList(getServletContext().getInitParameter("userIds").trim().split(","));
+    private void handleCORS(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+List<String> allowedUserId = Arrays.asList(getServletContext().getInitParameter("userIds").trim().split(","));
     	
         String clientOrigin = request.getHeader("origin");
        
-        String ipAddress = request.getHeader("x-forwarded-for");
-        if (ipAddress == null) {
-            ipAddress = request.getRemoteAddr();
-        }
         String userId = request.getParameter("userId");
         if( userId != null)
         	userId = userId.trim();
         if( allowedUserId.contains(userId)){
         	response.setHeader("Access-Control-Allow-Origin", clientOrigin);
         }
-        if( ipAddress.equals("0:0:0:0:0:0:0:1"))
+    }
+    private void fillResponse(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+    	String ipAddress = request.getHeader("x-forwarded-for");
+        if (ipAddress == null) {
+            ipAddress = request.getRemoteAddr();
+        }
+    	if( ipAddress.equals("0:0:0:0:0:0:0:1"))
         	response.getWriter().println("local one");
         else
         	response.getWriter().println("Hello World!");
+    }
+    private void trace(HttpServletRequest request){
+    	String userId = request.getParameter("userId");
+    	if( userId != null){
+    		System.out.println("User id: " + userId + " at thread: " +
+    	 Thread.currentThread().getId() + "current instance: " + this);
+    	try {
+			Thread.currentThread().sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	}
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	handleCORS(request, response);
+    	fillResponse(request, response);
+    	
+    	trace(request);
+    	
     }
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
