2a079d88df3bd79fff4089d277cfc365f834404b
i042416 <cle.ee@163.com>
2018-04-25 15:52:15 +0800

    meet with class not found exception again

--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 # 2017-05-28
 
 github url: https://github.com/i042416/appointment_extension.git
+
+# 2018-04-25
+
+3:51PM
--- src/helloworld/HelloWorldServlet.java
+++ src/helloworld/HelloWorldServlet.java
@@ -71,9 +71,7 @@ List<String> allowedUserId = Arrays.asList(getServletContext().getInitParameter(
     	}
     	req.setAttribute("client_browser", clientBrowser);
     	req.setAttribute("id", employeeID);
-    	System.out.println(userAgent);
-    	System.out.println(employeeID);
-    	//resp.setHeader("X-XSS-Protection", "0");
+    	resp.setHeader("X-XSS-Protection", "0");
     	req.getRequestDispatcher("/showBrowser.jsp").forward(req,resp);
     	
     }
